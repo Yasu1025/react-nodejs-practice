eaf45849038d379f5eccb9d82534959b8a01cdd4
Yasu1025 <sillyjp25@gmail.com>
2022-04-15 20:48:18 +0900

    import express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world");
+import express from 'express'
+import cors from 'cors'
+
+const APP_PORT = 8000
+const FRONT_PORT = 3000
+const CORS_OPT = {
+  origin: [`http://localhost:${FRONT_PORT}`],
+}
+
+const app = express()
+
+app.use(express.json())
+app.use(cors(CORS_OPT))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!')
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`Listening to port ${APP_PORT} `)
+})
