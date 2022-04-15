695e1b0118d3dfe8bcafa11f483f85718acf1ca1
Yasu1025 <sillyjp25@gmail.com>
2022-04-15 21:06:22 +0900

    create base routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { routes } from './route/routes'
 
 const APP_PORT = 8000
 const FRONT_PORT = 3000
@@ -12,9 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(CORS_OPT))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!')
-})
+routes(app)
 
 app.listen(APP_PORT, () => {
   console.log(`Listening to port ${APP_PORT} `)
