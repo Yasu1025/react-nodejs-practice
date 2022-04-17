12c64789381552bcb3f63615448bd5bd0d0cfaae
Yasu1025 <sillyjp25@gmail.com>
2022-04-17 14:42:16 +0900

    conect to mysql with typeORM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import 'dotenv/config'
 import cors from 'cors'
 import { routes } from './route/routes'
+import { createConnection } from 'typeorm'
+import ormConfig from './config/ormconfig'
 
 const APP_PORT = 8000
 const FRONT_PORT = 3000
@@ -8,13 +11,16 @@ const CORS_OPT = {
   origin: [`http://localhost:${FRONT_PORT}`],
 }
 
-const app = express()
+createConnection(ormConfig).then(connection => {
+  console.log(connection)
+  const app = express()
 
-app.use(express.json())
-app.use(cors(CORS_OPT))
+  app.use(express.json())
+  app.use(cors(CORS_OPT))
 
-routes(app)
+  routes(app)
 
-app.listen(APP_PORT, () => {
-  console.log(`Listening to port ${APP_PORT} `)
+  app.listen(APP_PORT, () => {
+    console.log(`Listening to port ${APP_PORT} `)
+  })
 })
