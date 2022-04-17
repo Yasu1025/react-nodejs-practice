34e98078221582ade1368b497263b830ee01f58a
Yasu1025 <sillyjp25@gmail.com>
2022-04-17 17:12:55 +0900

    secret key in .env

--- src/controller/auth_controller.ts
+++ src/controller/auth_controller.ts
@@ -42,7 +42,7 @@ export const Login = async (req: Request, res: Response) => {
   }
 
   const payload = { id: user.id }
-  const token = sign(payload, 'secret')
+  const token = sign(payload, process.env.SECRET_KEY)
 
   res.cookie('jwt', token, {
     httpOnly: true,
@@ -56,7 +56,7 @@ export const Login = async (req: Request, res: Response) => {
 export const AuthenticatedUser = async (req: Request, res: Response) => {
   try {
     const jwt = req.cookies['jwt']
-    const payload: any = verify(jwt, 'secret')
+    const payload: any = verify(jwt, process.env.SECRET_KEY)
     if (!payload) {
       return res.status(401).send({ message: 'Unauthenticated....' })
     }
